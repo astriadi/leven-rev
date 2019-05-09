aa887118827d1fbfe532efcad0a8520e9d6bcb69
Astriadi Setrawandana <astriadi@outlook.com>
2019-05-09 20:27:03 +0700

    Updates

---
+++ precache-manifest.15753a05e4b60206f071076377939391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f09819603aa8a922d64b792058c604b",
+    "url": "/leven-rev/index.html"
+  },
+  {
+    "revision": "8084da8d5b9acc01ecec",
+    "url": "/leven-rev/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/leven-rev/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8084da8d5b9acc01ecec",
+    "url": "/leven-rev/static/js/main.17764755.chunk.js"
+  },
+  {
+    "revision": "5455d375057796744c9e",
+    "url": "/leven-rev/static/js/runtime~main.15768fff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/leven-rev/static/media/logo.5d5d9eef.svg"
+  }
+]);
